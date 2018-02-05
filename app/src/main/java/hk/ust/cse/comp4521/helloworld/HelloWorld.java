03fd8dfe05e1b1eb6613b2eba79376bbe1aeea55
Jogesh Muppala <jmuppala@hotmail.com>
2018-02-05 15:43:48 +0800

    Hello Friend Part 2

--- app/src/main/java/hk/ust/cse/comp4521/helloworld/HelloWorld.java
+++ app/src/main/java/hk/ust/cse/comp4521/helloworld/HelloWorld.java
@@ -1,5 +1,6 @@
 package hk.ust.cse.comp4521.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class HelloWorld extends AppCompatActivity {
+public class HelloWorld extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,12 @@ public class HelloWorld extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button helloButton = (Button) findViewById(R.id.helloButton);
+        helloButton.setOnClickListener(this);
+
+        Button goodbyeButton = (Button) findViewById(R.id.goodbyeButton);
+        goodbyeButton.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +57,25 @@ public class HelloWorld extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText friendName = (EditText) findViewById(R.id.friendName);
+        String name = friendName.getText().toString();
+
+        Intent mIntent = new Intent(this, ShowMessage.class);
+
+        switch(view.getId()) {
+            case R.id.helloButton:
+                mIntent.putExtra("Friend", "Hello " + name + "!");
+                break;
+            case R.id.goodbyeButton:
+                mIntent.putExtra("Friend", "Goodbye " + name + "!");
+                break;
+            default:
+                break;
+        }
+        startActivity(mIntent);
     }
 }
---
+++ app/src/main/java/hk/ust/cse/comp4521/helloworld/ShowMessage.java
@@ -0,0 +1,36 @@
+package hk.ust.cse.comp4521.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class ShowMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent mIntent = getIntent();
+        String message = mIntent.getStringExtra("Friend");
+        TextView textMessage = (TextView) findViewById(R.id.textMessage);
+        textMessage.setText(message);
+    }
+
+}
